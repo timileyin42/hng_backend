d781dcd7b42c1e61b9125dcdb7fe114bd446eb3f
timileyin42 <akanmuibro@gmail.com>
2025-01-30 08:20:20 +0000

    cors handled

--- my-api/app.ts
+++ my-api/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
+app.use(cors());
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
     const response = {
